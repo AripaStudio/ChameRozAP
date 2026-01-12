979eb5f14caa62912b4e23579d08bf1a31bfc450
khashayar <aripastudio44@gmail.com>
2026-01-12 09:55:05 -0800

    Update History Chame

--- ChameRozAP/ServiceManager/GitManager.cs
+++ ChameRozAP/ServiceManager/GitManager.cs
@@ -42,7 +42,7 @@ namespace ChameRozAP.ServiceManager
             var LastPoem = AripaTools.GetNameAndTimeSpanOfLastCreatedFiles(HistoryFolderAddress);
             LastPoem.TimeSpanFile *= -1;
             ShowMessageAP.ShowMessageBoxAP($"CheckPastPoem  , TimeSpan : {LastPoem.TimeSpanFile.Days} --- Name : {LastPoem.NameFile}" , "Debug");
-            int Days = (LastPoem.TimeSpanFile.Days ) - 1;
+            int Days = LastPoem.TimeSpanFile.Days ;
             ShowMessageAP.ShowMessageBoxAP($"Show Days in CheckPastPoem : {Days.ToString()}", "Debug");
             if (Days > 0)
             {
